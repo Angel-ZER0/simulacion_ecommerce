75fdda99acd044582bc953b70f70374384b9f7ea
Angel-ZER0 <cora.angelandres@gmail.com>
2025-04-15 16:14:32 -0600

    paquete que contitne la clase main que ejecuta el proyecto además de añadir cietos registros a las tablas si se cumplen los requisitos para lo insertarlos

---
+++ src/test/java/angel_zero/inventario/InventarioBackendBaseDeDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package angel_zero.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioBackendBaseDeDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
