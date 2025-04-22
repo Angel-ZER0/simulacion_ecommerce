08673f9387a5196cae879f7e965e6bc538824742
Angel-ZER0 <cora.angelandres@gmail.com>
2025-04-22 16:00:40 -0600

    DTO para buscar tipo orden de productos por transacción

--- src/main/java/angel_zero/inventario/historialOrdenes/DTOCompraRealizada.java
+++ src/main/java/angel_zero/inventario/historialOrdenes/DTOCompraRealizada.java
@@ -2,14 +2,15 @@ package angel_zero.inventario.historialOrdenes;
 
 import java.math.BigDecimal;
 
-public record DTOCompraRealizada(Long id, String fecha, BigDecimal montoTotal) {
+public record DTOCompraRealizada(Long id, String fecha, BigDecimal montoTotal, String estadoPago) {
 
 	public DTOCompraRealizada(EntidadHistorialOrdenes ordenAnterior) {
 		
 		this(
 			ordenAnterior.getId(), 
 			ordenAnterior.getFechaFiniquitpOrden().toString().replace("T", " Hora: ").substring(0, 22), 
-			ordenAnterior.getTotalAPagar()
+			ordenAnterior.getTotalAPagar(),
+			ordenAnterior.getEstadoTransaccion().getEstado()
 		);
 		
 	}
